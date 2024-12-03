b3000e6901ea284f3a0bf0e5aa86a17329d06fed
3bdul1ah <a2053231@gmail.com>
2024-12-03 10:04:19 +0800

    abdullah updates

--- detection.py
+++ detection.py
@@ -1,7 +1,17 @@
+import cv2
+import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
-model = YOLO('runs/detect/train7/weights/best.pt')
-image = '/home/ubuntu/capstone/test/images/Good-Welding-images_47_jpeg_jpg.rf.82630d380c49fb0a597d7343a033ebf7.jpg'
-# image = '/home/ubuntu/capstone/test/images/bad_weld_vid92_jpeg_jpg.rf.ea3c805c99ab9ff65a27c65112e22f87.jpg'
+model = YOLO('best.pt')
 
-results = model.predict(source=image, save=True, show=True)
+image_path = '/home/ubuntu/Desktop/E1G2/WeldGuard/welding train/valid/images/22-possible-causes-of-weld-metal-porosity-weld-beads_jpg.rf.db243564f3559b3cb40704832e48f6eb.jpg'
+
+results = model.predict(source=image_path, save=False)
+
+annotated_frame = results[0].plot()
+
+annotated_frame_rgb = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+
+plt.imshow(annotated_frame_rgb)
+plt.axis('off')
+plt.show()
